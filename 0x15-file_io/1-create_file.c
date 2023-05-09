c2d5a2d5b183e76df0875b7173aa43563bf1e1a6
kennethmwangikihara <kennethmwangikihara>
2023-05-09 02:57:38 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * @create_file - Create file.
- * @filename: A pointer to name of the file to create.
- * @text_content: A pointer to  string to write to the file.
+ * create_file - Creates a file.
+ * @filename: pointer to the name of the file to create.
+ * @text_content: pointer to string to write to file..
  *
- * Return: If the function fails -1 otherwise -1
+ * Return: If the function fails - -1
+ *         Otherwise - 1...
  */
 
 int create_file(const char *filename, char *text_content)
