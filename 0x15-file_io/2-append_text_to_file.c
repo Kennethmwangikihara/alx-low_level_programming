1651753877ad69bfb8a868fab1907135c4cd1260
kennethmwangikihara <kennethmwangikihara>
2023-05-11 02:35:51 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 
 /**
  * append_text_to_file - appends text at the end of a file..
- * @filename:pointer to the name of the file..
+ * @filename:pointer to the name of the file...
  * @text_content:string to add to the end of the file..
  *
  * Return: If the function fails or filename is NULL - -1.
